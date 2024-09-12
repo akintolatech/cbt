db963abef03877ffba03fe45f0925e40ca6c7df1
akintolatech <akintolatechnologies@gmail.com>
2024-09-12 21:52:08 +0100

    Database Schema Development

--- authenticator/models.py
+++ authenticator/models.py
@@ -35,25 +35,24 @@ class StudentUserManager(BaseUserManager):
 
 
 class StudentUser(AbstractBaseUser, PermissionsMixin):
-    passport = models.ImageField(upload_to="passports", blank=True, null=True)
     username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['password']
+
+    passport = models.ImageField(upload_to="passports", blank=True, null=True)
     phone = models.CharField(max_length=11, blank=True, null=True)
 
-    classes = models.CharField(max_length=50)
-
+    classes = models.ForeignKey(ClassGroup, on_delete=models.CASCADE)
     form = models.ForeignKey(ClassGroup, on_delete=models.CASCADE)
+
     total_score = models.IntegerField(default=2)
     position = models.IntegerField(default=0)
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['password']
-
     objects = StudentUserManager()
 
     def __str__(self):
         return self.username
-
--- cbtapp/settings.py
+++ cbtapp/settings.py
@@ -129,5 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'authenticator.CustomUser'
-AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend', 'authenticator.backends.MyAuthBackend']
+# AUTH_USER_MODEL = 'authenticator.CustomUser'
+# AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend', 'authenticator.backends.MyAuthBackend']
