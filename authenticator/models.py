59f77e48bed45b83d7467ca4eef87b30ed479271
akintolatech <akintolatechnologies@gmail.com>
2024-09-12 21:45:45 +0100

    Database Schema Development

--- authenticator/models.py
+++ authenticator/models.py
@@ -4,6 +4,21 @@ from django import forms
 
 
 # Create your models here.
+
+
+class Form(models.Model):
+    name = models.CharField(max_length=33)
+    form_img = models.ImageField(upload_to='form_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ClassGroup(models.Model):
+    name = models.CharField(max_length=33)
+    class_form = models.ForeignKey(Form, on_delete=models.CASCADE)
+
+
 class StudentUserManager(BaseUserManager):
     def create_user(self, username, password=None):
         user = self.model(username=username)
@@ -26,6 +41,8 @@ class StudentUser(AbstractBaseUser, PermissionsMixin):
     phone = models.CharField(max_length=11, blank=True, null=True)
 
     classes = models.CharField(max_length=50)
+
+    form = models.ForeignKey(ClassGroup, on_delete=models.CASCADE)
     total_score = models.IntegerField(default=2)
     position = models.IntegerField(default=0)
 
@@ -40,10 +57,3 @@ class StudentUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.username
 
-
-class Form(models.Model):
-    name = models.CharField(max_length=50)
-    rnk = models.ImageField(upload_to='rnk/', null=True, blank=True)
-
-    def __str__(self):
-        return self.name
