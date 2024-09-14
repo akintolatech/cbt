3cbcc2236499f9884e0faf7717e086761d45468f
akintolatech <akintolatechnologies@gmail.com>
2024-09-14 23:35:52 +0100

    Static Files

---
+++ authenticator/backends.py
@@ -0,0 +1,20 @@
+# myapp/backends.py
+from django.contrib.auth.backends import BaseBackend
+from .models import StudentUser
+
+
+class MyAuthBackend(BaseBackend):
+
+    def authenticate(self, request, username=None, password=None, ):
+        try:
+            user = StudentUser.objects.get(username=username)
+            if user.password == password:
+                return user
+        except StudentUser.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return StudentUser.objects.get(pk=user_id)
+        except StudentUser.DoesNotExist:
+            return None
