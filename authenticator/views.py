b3e0cee1d8b05842ef62c10ccd5f624eec40eba7
akintolatech <akintolatechnologies@gmail.com>
2024-09-17 20:30:27 +0100

    Authenticator implementation

--- authenticator/views.py
+++ authenticator/views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 # Create your views here.
 def user_login(request):
     if request.method == "POST":
-        form = LoginForm(request.post)
+        form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(
@@ -21,21 +21,38 @@ def user_login(request):
                         "msg": "wired"
                     }
                     login(request, user)
-                    return render(request, "authenticator/student-dashboard.html", context)
+                    return render(request, "authenticator/dashboard.html", context)
                 else:
-                    return HttpResponse("Your Account is Inactive")
+                    return HttpResponse("You have been Banned from the system")
             else:
-                return HttpResponse("Invalid Login")
-        else:
-            form = LoginForm()
-        return render(request, "authenticator/login.html")
+                form = LoginForm()
+                return render(request, "authenticator/login.html", {"form": form, "error": "Invalid Login"})
+    else:
+        form = LoginForm()
+    return render(request, "authenticator/login.html", {"form": form})
 
 
 def register(request):
-    msg = "Wired"
-
-    context = {
-        "msg": msg
-    }
-
-    return render(request, "authenticator/register.html", context)
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # Save the User object
+            new_user.save()
+            return render(
+                request,
+                'account/register_done.html',
+                {'new_user': new_user}
+            )
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'account/register.html',
+        {'user_form': user_form}
+    )
