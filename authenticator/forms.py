e1986ba58ea9640fca70445f6ee2716533184c1a
akintolatech <akintolatechnologies@gmail.com>
2024-09-19 00:36:21 +0100

    Tester Implementation

--- authenticator/forms.py
+++ authenticator/forms.py
@@ -4,12 +4,26 @@ from .models import Profile, ClassArm
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your username'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Enter your SVC No (00/0000 format)',
+                'pattern': r'\d{2}/\d{4}'
+            }
+        )
+    )
+
+    # Remove labels by setting them to an empty string
+    labels = {
+        'username': "",
+        'password': "",
+    }
 
 
 class UserRegistrationForm(forms.ModelForm):
-
     class_arm = forms.ModelChoiceField(
         queryset=ClassArm.objects.all(),
         required=True,
@@ -19,7 +33,25 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'password']
-        # fields = ['username', 'first_name', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Enter your username'}),
+            'password': forms.PasswordInput(
+                attrs={
+                    'placeholder': 'Enter your SVC No (00/0000 format)',
+                    'pattern': r'\d{2}/\d{4}'
+                }
+            ),
+        }
+        labels = {
+            'username': '',
+            'password': '',
+        }
+
+        # Remove help text for all fields by setting an empty dictionary
+        help_texts = {
+            'username': None,
+            'password': None,
+        }
 
 
 class UserEditForm(forms.ModelForm):
@@ -38,52 +70,3 @@ class ProfileEditForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ProfileEditForm, self).__init__(*args, **kwargs)
         self.fields['photo'].widget.attrs.update({'id': 'image_input_id'})
-
-# class NameForm(forms.ModelForm):
-#     CHOICES = (
-#
-#         ('JSS1 A', 'JSS1 A'),
-#         ('JSS1 B', 'JSS1 B'),
-#         ('JSS1 C', 'JSS1 C'),
-#         ('JSS1 D', 'JSS1 D'),
-#         ('JSS1 E', 'JSS1 E'),
-#
-#         ('JSS2 A', 'JSS2 A'),
-#         ('JSS2 B', 'JSS2 B'),
-#         ('JSS2 C', 'JSS2 C'),
-#         ('JSS2 D', 'JSS2 D'),
-#         ('JSS2 E', 'JSS2 E'),
-#
-#         ('JSS3 A', 'JSS3 A'),
-#         ('JSS3 B', 'JSS3 B'),
-#         ('JSS3 C', 'JSS3 C'),
-#         ('JSS3 D', 'JSS3 D'),
-#         ('JSS3 E', 'JSS3 E'),
-#         ('JSS3 F', 'JSS3 F'),
-#
-#         ('SSS1 ART', 'SSS1 ART'),
-#         ('SSS1 COMMERCIAL', 'SSS1 COMMERCIAL'),
-#         ('SSS1 SCIENCE A', 'SSS1 SCIENCE A'),
-#         ('SSS1 SCIENCE B', 'SSS1 SCIENCE B'),
-#         ('SSS1 TECHNICAL A', 'SSS1 TECHNICAL A'),
-#         ('SSS1 TECHNICAL B', 'SSS1 TECHNICAL B'),
-#
-#         ('SSS2 ARTCOMM', 'SSS2 ARTCOMM'),
-#         ('SSS2 SCIENCE A', 'SSS2 SCIENCE A'),
-#         ('SSS2 SCIENCE B', 'SSS2 SCIENCE B'),
-#         ('SSS2 TECHNICAL A', 'SSS2 TECHNICAL A'),
-#         ('SSS2 TECHNICAL B', 'SSS2 TECHNICAL B'),
-#
-#         ('SSS3 ART', 'SSS3 ART'),
-#         ('SSS3 COMMERCIAL', 'SSS3 COMMERCIAL'),
-#         ('SSS3 SCIENCE', 'SSS3 SCIENCE'),
-#         ('SSS3 TECHNICAL', 'SSS3 TECHNICAL'),
-#         ('SSS3 SCIENCE AND TECHNICAL', 'SSS3 SCIENCE AND TECHNICAL'),
-#
-#     )
-#
-#     classes = forms.ChoiceField(choices=CHOICES, widget=forms.Select)
-#
-#     class Meta:
-#         model = StudentUser
-#         fields = ('username', 'password', 'classes')
